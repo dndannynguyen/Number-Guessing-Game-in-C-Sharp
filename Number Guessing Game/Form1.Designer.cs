7a8609c44670adbc2005f41e9d62d68152990bb8
dndannynguyen <dn.dannynguyen@hotmail.com>
2024-01-17 20:14:24 -0800

    Add project files.

---
+++ Number Guessing Game/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Number_Guessing_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCheckAnswer = new System.Windows.Forms.Button();
+            this.labelTop = new System.Windows.Forms.Label();
+            this.labelBottom = new System.Windows.Forms.Label();
+            this.textBoxInputAnswer = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonCheckAnswer
+            // 
+            this.buttonCheckAnswer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.buttonCheckAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCheckAnswer.Location = new System.Drawing.Point(383, 280);
+            this.buttonCheckAnswer.Name = "buttonCheckAnswer";
+            this.buttonCheckAnswer.Size = new System.Drawing.Size(289, 87);
+            this.buttonCheckAnswer.TabIndex = 0;
+            this.buttonCheckAnswer.Text = "Check";
+            this.buttonCheckAnswer.UseVisualStyleBackColor = false;
+            this.buttonCheckAnswer.Click += new System.EventHandler(this.checkNumber);
+            // 
+            // labelTop
+            // 
+            this.labelTop.AutoSize = true;
+            this.labelTop.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTop.Location = new System.Drawing.Point(284, 48);
+            this.labelTop.Name = "labelTop";
+            this.labelTop.Size = new System.Drawing.Size(499, 31);
+            this.labelTop.TabIndex = 1;
+            this.labelTop.Text = "Guess the number between: 0 and 20";
+            this.labelTop.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelBottom
+            // 
+            this.labelBottom.AutoSize = true;
+            this.labelBottom.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBottom.Location = new System.Drawing.Point(462, 427);
+            this.labelBottom.Name = "labelBottom";
+            this.labelBottom.Size = new System.Drawing.Size(148, 31);
+            this.labelBottom.TabIndex = 2;
+            this.labelBottom.Text = "Attempt: 1";
+            // 
+            // textBoxInputAnswer
+            // 
+            this.textBoxInputAnswer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.textBoxInputAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxInputAnswer.Location = new System.Drawing.Point(333, 146);
+            this.textBoxInputAnswer.MaxLength = 2;
+            this.textBoxInputAnswer.Name = "textBoxInputAnswer";
+            this.textBoxInputAnswer.Size = new System.Drawing.Size(380, 38);
+            this.textBoxInputAnswer.TabIndex = 3;
+            this.textBoxInputAnswer.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.Gold;
+            this.ClientSize = new System.Drawing.Size(1048, 505);
+            this.Controls.Add(this.textBoxInputAnswer);
+            this.Controls.Add(this.labelBottom);
+            this.Controls.Add(this.labelTop);
+            this.Controls.Add(this.buttonCheckAnswer);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Number Guessing Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCheck;
+        private System.Windows.Forms.TextBox textBoxGuess;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxAnswer;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonCheckAnswer;
+        private System.Windows.Forms.Label labelTop;
+        private System.Windows.Forms.Label labelBottom;
+        private System.Windows.Forms.TextBox textBoxInputAnswer;
+    }
+}
+
---
+++ Number Guessing Game/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Number_Guessing_Game
+{
+    public partial class Form1 : Form
+    {
+        Random randomNumber = new Random();
+
+        int number = 0;
+        int guesses = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.textBoxInputAnswer.KeyPress += new KeyPressEventHandler(textBoxInputAnswer_KeyPress);
+        }
+
+        private void checkNumber(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(textBoxInputAnswer.Text);
+
+            guesses += 1;
+            labelBottom.Text = "Attempt: " + guesses;
+
+            if (i == number)
+            {
+                MessageBox.Show("You guessed it. Try another.");
+                loadQuestions();
+                textBoxInputAnswer.Text = "";
+                guesses = 0;
+                labelBottom.Text = "Attempt: " + guesses;
+            }
+            else if (i < number)
+            {
+                MessageBox.Show("Go Higher");
+                textBoxInputAnswer.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Go Lower");
+                textBoxInputAnswer.Text = "";
+            }
+        }
+
+        private void loadQuestions()
+        {
+            number = randomNumber.Next(0, 20);
+        }
+
+        private void textBoxInputAnswer_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !(char.IsDigit(e.KeyChar) || e.KeyChar == 8);
+        }
+    }
+}
